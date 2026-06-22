8c27f5ac4756ba8532c99ef5df62131fb55d032e
RivLaura <rmlaura261114@gmail.com>
2026-06-22 17:01:32 -0300

    COD-124 Integración del sistema de seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DeveloperSeeder::class,
             ProjectSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
